86edbf65b0a8ebe26b7205b39a3f7c00de3fb00a
sermejia <sermejia@uamv.edu.ni>
2025-03-18 14:27:12 -0600

    commit

---
+++ Curso Dalto/operadores-de-comparacion.py
@@ -0,0 +1,19 @@
+igual_que = 5 == 5
+
+distinto_de = 5 != 12
+
+mayor_que = 5 > 3
+
+menor_que = 3 < 5
+
+menor_o_igual = 5 <= 6
+
+mayor_o_igual = 5 >= 5
+
+print(igual_que)
+print(distinto_de)
+print(mayor_que)
+print(menor_que)
+print(menor_o_igual)
+print(mayor_o_igual)
+
