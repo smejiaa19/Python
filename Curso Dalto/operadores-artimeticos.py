107e38666daa37a5c82e24a01e704c25acaad79f
sermejia <sermejia@uamv.edu.ni>
2025-03-18 14:20:08 -0600

    tercer commit

--- Curso Dalto/operadores-artimeticos.py
+++ Curso Dalto/operadores-artimeticos.py
@@ -1,1 +1,25 @@
 #suma y resta (+ y -)
+suma = 12 + 5
+resta = 12 - 5
+
+#multiplicacion y division (* y /)
+multi = 12 * 5
+divi = 12 / 5 #Devuelve un dato flotante (float)
+
+#potenciacion (exponente) (**)
+exponente = 12**5
+#Aqui dijimos 12 elevado a la 5
+
+#Division baja (//) devuyelve entero redondeado hacia abajo
+division_baja = 12 // 5
+
+#Modular o resto (%)
+modular = 12 % 5
+
+print(suma)
+print(resta)
+print(multi)
+print(divi)
+print(exponente)
+print(division_baja)
+print(modular)
