fbbfa800015c3c7b6ad486248668bed28a0dc213
sermejia <sermejia@uamv.edu.ni>
2025-03-18 12:34:45 -0600

    primer commit

---
+++ Curso Dalto/tipos-de-datos.py
@@ -0,0 +1,23 @@
+"string" 
+'string'
+
+"""string"""
+'''string'''
+
+#Asi podemos representar las cadenas de texto  
+#La diferencia es que si queremos escribir texto en una sola linea debemos hacerlo como los dos primeros
+#Si queremos escribir texto en varias lineas debemos hacerlo como los dos ultimos, ejemplo: 
+
+'''Hola tu nombre es: 
+    Nombre: Silvio
+    Apellido: Mejia
+'''
+
+#Numeros pueden ser enteros y flotantes / int o float
+40 
+40.5
+
+#Booleanos puede ser true o false
+False
+True
+
