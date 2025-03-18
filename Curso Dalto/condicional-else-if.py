9adc94a424a1f9a1f138cf2fa4f54651aaa4dbfa
sermejia <sermejia@uamv.edu.ni>
2025-03-18 16:50:04 -0600

    ultimo commit del curso por hoy

---
+++ Curso Dalto/condicional-else-if.py
@@ -0,0 +1,8 @@
+ingreso_mensual = 500
+
+if ingreso_mensual > 10000: 
+    print("Estas bien en cualquier parte del mundo")
+elif ingreso_mensual > 1000:
+    print("Estas bien economicamente en LATAM")
+else:
+    print("Sos pobre")
---
+++ Curso Dalto/condicionales-if-else.py
@@ -0,0 +1,17 @@
+#if - else 
+
+edad = 10
+
+if edad >= 18:
+    print("Podes pasar")
+else: 
+    print("No podes pasar")
+    
+new_password = "1234"
+password = "12345"
+
+if new_password == password:
+    print("Contraseña correcta")
+else:
+    print("Contraseña incorrecta")
+   
