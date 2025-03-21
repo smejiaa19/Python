2f312cff8886ca9fe68d2f4119071c87819ba73b
smejiaa19 <sermejia@uamv.edu.ni>
2025-03-21 07:22:18 -0600

    pruebas

--- Ejercicios-practica/año-bisiesto.py
+++ Ejercicios-practica/año-bisiesto.py
@@ -1,10 +1,21 @@
 # Para determinar si un año es bisiesto, aun sigue en desarrollo.
 # Practicar bucles 
 
-x = int(input('Ingrese el año: '))
-operacion = x / 4
-
-if type(operacion)==int:
+fecha = int(input('Ingrese el año que desea verificar: '))
+if fecha / 100 == type(fecha)== int and fecha / 400 == type(fecha) == int:
     print('El año es bisiesto')
 else: 
     print('El año no es bisiesto')
+    
+'''La logica quiere decir que un año debe ser divisible entre 4
+con un resto de 0 pero que no debe ser axactamente divisible entre 100
+Despues debemos de verificar si la fecha es exactamente divisible entre 400
+Si un año es exactamente divisible entre 100 pero no entre 400, entonces 
+no es un año bisiesto. En cambio si es divisible entre 100 y 400 si es bisiesto
+
+Podemos utilizar bucles para definir 
+ejemplo: Si fecha / 100 == 0 and fecha // 400 == 0:
+    print('El año es bisiesto')
+else:
+    print('El año no es bisiesto')
+'''
