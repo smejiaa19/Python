faa58421fdf537f5bb003a0c2eddeadc19c4f162
smejiaa19 <sermejia@uamv.edu.ni>
2025-03-21 07:57:37 -0600

    WaoS

---
+++ Ejercicios-practica/area-triangulo.py
@@ -0,0 +1,6 @@
+# Calcular el area de un triangulo a partir de su base y altura.
+base = int(input('Ingrese la base del triangulo: '))
+altura = int(input('Ingrese a altura del triangulo: '))
+area = (base * altura) / 2
+
+print(f'El area del triangulo con base {base} cm y altura {altura} cm es igual a: {area} cm2')
---
+++ Ejercicios-practica/promedio-calificaciones.py
@@ -0,0 +1,7 @@
+# Calcular el promedio de una lista de calificaciones
+
+contador = int(input('Cuantas clases desea ingresar: '))
+i = 0
+while i <= contador:
+    calificacion = int(input("Ingrese la calificacion de la clase: "))
+    
