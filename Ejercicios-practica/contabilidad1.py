f9ed680d6849b3052ee98933b624981547ddd6b5
smejiaa19 <sermejia@uamv.edu.ni>
2025-03-28 09:21:59 -0600

    Holaaaa

---
+++ Ejercicios-practica/contabilidad1.py
@@ -0,0 +1,9 @@
+ingresos = int(input('Ingrese el total de ingresos: '))
+gastos = int(input('Ingrese el total de gastos: '))
+
+if ingresos > gastos:
+    print('La empresa tiene utilidades')
+else: 
+    print('La empresa tiene perdidas')
+    
+    
